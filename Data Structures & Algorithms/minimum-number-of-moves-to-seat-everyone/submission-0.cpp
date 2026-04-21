664138aa6eb23071d71101379b885c86450833ae
Aman Shankar singh <96378145+singhAman05@users.noreply.github.com>
2026-04-22 01:07:46 +0530

    Add: minimum-number-of-moves-to-seat-everyone - submission-0

---
+++ Data Structures & Algorithms/minimum-number-of-moves-to-seat-everyone/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        int n = seats.size();
+        sort(students.begin(), students.end());
+        sort(seats.begin(), seats.end());
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans += abs(students[i]-seats[i]);
+        }
+        return ans;
+
+        // 1 2 4
+        // 1 3 3
+    }
+};
