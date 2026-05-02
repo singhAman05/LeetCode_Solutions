a3cb07ed279787f539001a0352a855e6b4f4cca5
Aman Shankar singh <96378145+singhAman05@users.noreply.github.com>
2026-05-02 18:07:46 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int a = cost[0], b = cost[1];
+        for(int i = 2; i < n; i++){
+            int c = cost[i] + min(a, b);
+            a = b;
+            b = c;
+        }
+
+        return min(a, b);
+    }
+};
